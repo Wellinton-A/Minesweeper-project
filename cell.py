a6b0b11c5664ca957b6119568aa4cfa6cdda7a63
wellinton <wellinton1067@gmail.com>
2023-01-14 14:39:08 -0300

    finishing project

--- cell.py
+++ cell.py
@@ -1,11 +1,19 @@
-from tkinter import Button
+from tkinter import Button, Label, messagebox
+from PIL import Image, ImageTk
 import random
 import settings
+import sys
+import time
+
 
 class Cell():
     all = []
-    def __init__(self, x, y, is_mine=False):
+    cell_count = settings.TOTAL_CELL_QUANTITY
+    cell_count_object = None
+    def __init__(self, x, y, is_mine=False, is_opened = False, marked_as_mine = False):
         self.is_mine = is_mine
+        self.marked_as_mine = marked_as_mine
+        self.is_opened = is_opened
         self.cell_btn_object = None
         self.x = x
         self.y = y
@@ -14,45 +22,100 @@ class Cell():
     def create_btn_object(self, location):
         btn = Button(
             location,
+            bg='#f0f0f3',
             width=settings.CELL_SIZE_WIDTH,
             height=settings.CELL_SIZE_HEIGHT,
-            )
+        )
         btn.bind('<Button-1>', self.left_click_actions)
         btn.bind('<Button-3>', self.right_click_actions)
         self.cell_btn_object = btn
+
+    @staticmethod
+    def create_label_cell_count(location):
+        label = Label(
+            location,
+            bg='black',
+            fg='white',
+            font=('', 30),
+            text=f'cell left: {Cell.cell_count}'
+        )
+        Cell.cell_count_object = label
 
     def left_click_actions(self, event):
         if self.is_mine:
             self.show_mine()
         else:
             self.show_cell()
-    
-    def show_mine(self):
-        self.cell_btn_object.configure(bg='red')
+            if self.cell_mine_surrounded_length == 0:
+                for cell in self.cell_surrounded_list:
+                        cell.show_cell()
 
-    def cell_aroud(self, x, y):
+
+    def show_mine(self):
         for i in Cell.all:
-            if i.x == x and i.y == y and i.x != None:
+            if i.is_mine:
+                i.cell_btn_object.configure(bg='red')
+        self.cell_btn_object.configure(bg='red')
+        messagebox.showwarning("GAME OVER", "You clicked on a mine!")
+        sys.exit()
+
+
+    def cell_around(self, x, y):
+        for i in Cell.all:
+            if i.x == x and i.y == y:
                 return i
 
-    def show_cell(self):
+    @property
+    def cell_surrounded_list(self):
         cell_list = []
-        cell_list.append(self.cell_aroud(self.x-1, self.y-1))
-        cell_list.append(self.cell_aroud(self.x, self.y-1))
-        cell_list.append(self.cell_aroud(self.x+1, self.y-1))
-        cell_list.append(self.cell_aroud(self.x-1, self.y))
-        cell_list.append(self.cell_aroud(self.x+1, self.y))
-        cell_list.append(self.cell_aroud(self.x-1, self.y+1))
-        cell_list.append(self.cell_aroud(self.x, self.y+1))
-        cell_list.append(self.cell_aroud(self.x+1, self.y+1))
-        mine_count = 0
-        for i in cell_list:
-            if i!=None and i.is_mine:
-                mine_count+=1
-        return self.cell_btn_object.configure(text=f'{mine_count}')
-    
+        cell_list.append(self.cell_around(self.x-1, self.y-1))
+        cell_list.append(self.cell_around(self.x, self.y-1))
+        cell_list.append(self.cell_around(self.x+1, self.y-1))
+        cell_list.append(self.cell_around(self.x-1, self.y))
+        cell_list.append(self.cell_around(self.x+1, self.y))
+        cell_list.append(self.cell_around(self.x-1, self.y+1))
+        cell_list.append(self.cell_around(self.x, self.y+1))
+        cell_list.append(self.cell_around(self.x+1, self.y+1))
+        cell_list = [cell for cell in cell_list if cell is not None]
+        return cell_list
+
+    @property
+    def cell_mine_surrounded_length(self):
+        counter = 0
+        for i in self.cell_surrounded_list:
+            if i.is_mine:
+                counter += 1
+        return counter
+
+    def show_cell(self):
+        if not self.is_opened:
+            Cell.cell_count -= 1
+            self.cell_btn_object.configure(text=f'{self.cell_mine_surrounded_length}',bg='#f0f0f3')
+            if Cell.cell_count_object:
+                Cell.cell_count_object.configure(text=f'Cell Left: {Cell.cell_count}')
+        self.is_opened = True
+        is_mine_cell = 0
+        for i in Cell.all:
+            if i.is_mine:
+                is_mine_cell += 1
+        self.cell_btn_object.unbind('<Button-1>')
+        self.cell_btn_object.unbind('<Button-3>')
+        if Cell.cell_count == is_mine_cell:
+            for i in Cell.all:
+                if i.is_mine:
+                    i.cell_btn_object.configure(bg='red')
+            messagebox.showinfo("Congrats!!", "You Won!!!")
+            sys.exit()         
+
     def right_click_actions(self, event):
-        print(f'I am {self.x},{self.y},{self.is_mine} right clicked')
+        if not self.is_opened:
+            if not self.marked_as_mine:
+                self.cell_btn_object.configure(bg='orange')
+                self.marked_as_mine = True
+            else:
+                self.cell_btn_object.configure(bg='#f0f0f3')
+                self.marked_as_mine = False
+
 
     @staticmethod
     def randomize_cells():
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from PIL import Image, ImageTk
 import settings
 import utils
 from cell import Cell
@@ -15,8 +16,16 @@ top_frame = Frame(
     width=utils.width_prct(100),
     height=utils.height_prct(25)
 )
- 
+
 top_frame.place(x=0, y=0)
+
+game_title = Label(
+    top_frame,
+    bg='black',
+    fg='white',
+    text='Minesweeper',
+    font=('', 48)
+).place(x=0, y=0)
 
 left_frame = Frame(
     root,
@@ -25,7 +34,7 @@ left_frame = Frame(
     height=utils.height_prct(75)
 )
 
-left_frame.place(x=0, y=180)
+left_frame.place(x=0, y=utils.height_prct(25))
 
 center_frame = Frame(
     root,
@@ -34,15 +43,20 @@ center_frame = Frame(
     height=utils.height_prct(75)
 )
 
-center_frame.place(x=360, y=180)
+center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
 
 for x in range(settings.GRID_WIDTH_SIZE):
     for y in range(settings.GRID_HEIGHT_SIZE):
-        c = Cell(x,y)
+        c = Cell(x, y)
         c.create_btn_object(center_frame)
         c.cell_btn_object.grid(
             column=x, row=y
         )
 
+Cell.create_label_cell_count(left_frame)
+Cell.cell_count_object.place(x=0, y=0)
+
+
 Cell.randomize_cells()
+
 root.mainloop()
